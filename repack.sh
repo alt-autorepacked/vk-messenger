fea85c14e88c2440d8092189dccdc2e81777431f
Maxim Slipenko <no-reply@maxim.slipenko.com>
2024-01-27 14:03:37 +0300

    wip

--- repack.sh
+++ repack.sh
@@ -47,7 +47,7 @@ _download() {
     epm -y repack "https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/latest/vk-messenger.rpm"
 }
 
-# _download
+_download
 download_version=$(_check_version_from_download)
 remote_version=$(_check_version_from_remote)
 
